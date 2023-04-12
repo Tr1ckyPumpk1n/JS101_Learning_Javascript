cece5c623e6576e36a2840856977127e783ed5e8
Tr1ckyPumpk1n <v.d.daniel.9@gmail.com>
2023-04-12 16:31:07 +0530

    Day 4

---
+++ Day 4/Problem3.js
@@ -0,0 +1,5 @@
+let character = 'h';
+
+if (!(character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u')) {
+  console.log("Consonent");
+}
---
+++ Day 4/Problem4.js
@@ -0,0 +1,5 @@
+let a = 10;
+let b = 21;
+let c = 11;
+
+(a > b && a > c) ? console.log("a is greatest") : (b > c && b > a) ? console.log("b is greatest") : console.log("c is greatest");
